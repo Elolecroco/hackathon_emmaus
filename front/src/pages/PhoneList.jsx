80d2c00415b60994172b9539e93c94c37b2db777
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-06-28 22:02:24 +0200

    add previous and next buttons on phone list

--- front/src/pages/PhoneList.jsx
+++ front/src/pages/PhoneList.jsx
@@ -7,12 +7,37 @@ import { useEffect, useState } from "react";
 const PhoneList = () => {
   const [phones, setPhones] = useState([]);
 
+  const resultsPerPage = 10; // Change this value as per your requirement
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const startIndex = (currentPage - 1) * resultsPerPage;
+  const endIndex = startIndex + resultsPerPage;
+  const currentResults = phones.slice(startIndex, endIndex);
+
   useEffect(() => {
     axios
       .get(`http://localhost:5080/api/phones`)
       .then((res) => res.data)
       .then((data) => setPhones(data));
   }, []);
+
+  useEffect(() => {
+    const calculatedTotalPages = Math.ceil(phones / resultsPerPage);
+    setTotalPages(calculatedTotalPages);
+  }, [phones.length, resultsPerPage]);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handlePreviousClick = () => {
+    handlePageChange(currentPage - 1);
+  };
+
+  const handleNextClick = () => {
+    handlePageChange(currentPage + 1);
+  };
 
   return (
     <div>
@@ -29,12 +54,35 @@ const PhoneList = () => {
               <div className="storage">Stockage</div>
               <div className="ram">RAM</div>
             </div>
-            {phones &&
-              phones.map((phone) => (
+
+            {currentResults &&
+              currentResults.map((phone) => (
                 <div className="line" key={phone.id}>
                   <Phones props={phone} />
                 </div>
               ))}
+          </div>
+          <div>
+            <button onClick={handlePreviousClick} disabled={currentPage === 1}>
+              Previous
+            </button>
+
+            {Array.from({ length: totalPages }, (_, index) => (
+              <button
+                key={index + 1}
+                onClick={() => handlePageChange(index + 1)}
+                disabled={currentPage === index + 1}
+              >
+                {index + 1}
+              </button>
+            ))}
+
+            <button
+              onClick={handleNextClick}
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </button>
           </div>
         </div>
       </div>
