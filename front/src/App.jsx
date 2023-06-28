8cee0cc3941277e56f145814715691152ecd167f
ELOISE BROCHIER <eloise.brochier@gmail.com>
2023-06-28 16:45:43 +0200

    components, pages, assets

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,16 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
 import './App.css'
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
