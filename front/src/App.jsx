f8a554aa44777cac644036434ed72d65c1ea6413
Olivier Gomez <ashitaaaka@gmail.com>
2023-06-28 20:10:42 +0200

    create form & css

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import PhoneSurvey from './pages/PhoneSurvey'
+import AddPhone from './pages/AddPhone';
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/phonesurvey' element={<PhoneSurvey/>} />
+        <Route path='/addphone' element={<AddPhone/>} />
       </Routes>
     </Router>
   )
---
+++ front/src/pages/AddPhone.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+//import components
+import Navbar from '../components/Navbar/Navbar'
+//import css
+import './addPhone.css'
+//import assets
+import illustration from '../assets/ajouter-mobile_illus.png'
+
+const AddPhone = () => {
+  return (
+    <div className='add_phone_page'>
+        <div className="bg_decoration"></div>
+            <div className="add_phone_box">
+                <div className="image_bloc">
+                    <img src={illustration} alt="Add " />
+                </div>
+                <div className="content_bloc">
+                    <div className="title">
+                        <h1>
+                            Ajouter un smartphone
+                        </h1>
+                        <p className='subtitle'>Veuillez renseigner les champs ci-dessous</p>
+                    </div>
+                    <form action="">
+                        <div className="field">
+                            <label htmlFor="brand">Marque</label>
+                            <input type="text" name="brand" id="brand" placeholder='Marque'/>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="model">Modèle</label>
+                            <input type="text" name="model" id="model" placeholder='Modèle'/>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="storage">Stockage</label>
+                            <input type="text" name="storage" id="storage" placeholder='Mémoire de stockage'/>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="ram">Mémoire RAM</label>
+                            <input type="text" name="ram" id="ram" placeholder='Mémoire RAM'/>
+                        </div>
+                        <button type='submit'>Ajouter un smartphone</button>
+                    </form>
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default AddPhone
