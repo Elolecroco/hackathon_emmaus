8ff031c9c0c7205bcae6cb45f459e0adee536981
Olivier Gomez <ashitaaaka@gmail.com>
2023-06-29 15:34:29 +0200

    css corrections on home, navbar and phonelist

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -11,14 +11,18 @@ const Home = () => {
                     <div className='estimation-img-container'>
                         <img src={estimation} alt='illustration estimation' />
                     </div>
-                    <Link to='/phonesurvey'>Estimer un smartphone</Link>
+                    <div className="nav_button">
+                        <Link to='/phonesurvey'>Estimer un smartphone</Link>
+                    </div>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia inventore minima nobis cumque cum ullam itaque reprehenderit dolores. Temporibus minus neque rem quia voluptas dolorum quam assumenda non error eius.</p>
                 </div>
                 <div className='add-container'>
                     <div className='ajouter-img-container'>
                         <img src={ajouter} alt='illustration ajout' />
                     </div>
-                    <Link>Ajouter un smartphone</Link>
+                    <div className="nav_button">
+                        <Link to='/history'>Liste de nos smartphones</Link>
+                    </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, autem id quaerat a, maxime vel totam nostrum quia rem similique tempore sit sed, error inventore nisi cupiditate esse! Delectus, quod.</p>
                 </div>
             </div>
