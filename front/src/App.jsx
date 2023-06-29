6bee5faa28adf9fb229bdcc59ffe981b9190da3b
Romish-64 <valotromain6417@gmail.com>
2023-06-30 00:11:07 +0200

    accordion

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -10,6 +10,7 @@ import AddPhone from './pages/AddPhone';
 import Footer from './components/Footer/Footer';
 import PhoneList from "./pages/PhoneList";
 import HistoryList from "./pages/HistoryList";
+import AccordionDone from "./components/Accordion/Accordion";
 
 
 
@@ -20,9 +21,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/phonesurvey" element={<PhoneSurvey />} />
-        <Route path="/addphone" element={<AddPhone/>} />
+        <Route path="/addphone" element={<AddPhone />} />
         <Route path="/phonelist" element={<PhoneList />} />
         <Route path="/history" element={<HistoryList />} />
+        <Route path="/faq" element={<AccordionDone />} />
       </Routes>
       <Footer />
     </Router>
