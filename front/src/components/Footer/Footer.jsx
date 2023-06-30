00b59ee2ed3b2e31b4a5cb66ce5aadb94f88a468
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-06-30 09:49:31 +0200

    Footer with FAQ

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -1,16 +1,25 @@
-import { Link } from 'react-router-dom';
-import {FaFacebook, FaLinkedin, FaInstagram, FaTwitter} from 'react-icons/fa'
-import logo_ver2 from '../../assets/logo_emmaus_connect_ver2.png'
-import './Footer.css';
+import { Link } from "react-router-dom";
+import { FaFacebook, FaLinkedin, FaInstagram, FaTwitter } from "react-icons/fa";
+import logo_ver2 from "../../assets/logo_emmaus_connect_ver2.png";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <div className='footer-container'>
-            <div className='footer-logo-container'>
-                <img src={logo_ver2} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="footer-container">
+      <div className="footer-logo-container">
+        <img src={logo_ver2} />
+      </div>
+      <div className="footer-faq">
+        <Link to="/faq">FAQ</Link>
+        <p>Mentions legales</p>
+        <br />
+        <p>
+          © - Éloïse Brochier - Romain VALOT - Eric GODEFROY - Olivier GOMEZ -
+          Martin NOËL
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
